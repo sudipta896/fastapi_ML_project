6dc80fb024bfaf38b7aa7d96c0c5a2f712d711d6
sudipta896 <sudiptamondal86106@gmail.com>
2026-05-14 12:41:15 +0530

    added app configuration

--- app/core/security.py
+++ app/core/security.py
@@ -7,6 +7,7 @@ def create_token(data:dict,expire_minutes= 30):
     expire = datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+
 def verify_token(token:str):
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
